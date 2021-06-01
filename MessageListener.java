b29156703a6d8994fff406779b0001871216c8a3
Anthony Segedi <79811294+segedi-UW@users.noreply.github.com>
2021-06-01 13:44:21 -0500

    Required for Program

---
+++ MessageListener.java
@@ -0,0 +1,10 @@
+/**
+ * An interface designed to allow the notification of a new message
+ * 
+ * @author youngAgFox
+ *
+ */
+public interface MessageListener {
+
+	public void messageRecieved(String message);
+}
